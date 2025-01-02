b23dcaaf5b3fea5d989b7f24d097ac50da229159
Fynn Koch <mail@fynn-koch.de>
2025-01-02 07:06:46 +0100

    Upgrade to Angular 19

--- src/app/views/home-page/home-page.component.ts
+++ src/app/views/home-page/home-page.component.ts
@@ -1,7 +1,6 @@
 import {NgOptimizedImage} from '@angular/common';
 import {Component} from '@angular/core';
 import {MatGridList, MatGridTile,} from '@angular/material/grid-list';
-import {NavigationComponent} from '../../shared-components/navigation/navigation.component';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +8,6 @@ import {NavigationComponent} from '../../shared-components/navigation/navigation
   imports: [
     MatGridList,
     MatGridTile,
-    NavigationComponent,
     NgOptimizedImage,
   ],
   templateUrl: './home-page.component.html',
