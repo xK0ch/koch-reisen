3931d3391864fddf8e04a72337a7193d4ae087a4
Fynn Koch <mail@fynn-koch.de>
2025-07-02 07:45:13 +0200

    Redesign Home page

--- src/app/views/home-page/home-page.component.ts
+++ src/app/views/home-page/home-page.component.ts
@@ -1,15 +1,10 @@
 import {NgOptimizedImage} from '@angular/common';
 import {Component} from '@angular/core';
-import {MatGridList, MatGridTile,} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [
-    MatGridList,
-    MatGridTile,
-    NgOptimizedImage,
-  ],
+  imports: [NgOptimizedImage],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
